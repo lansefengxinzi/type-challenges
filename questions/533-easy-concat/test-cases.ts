17da61f15095199ee552be7940eddd8efc42d306
lansefengxinzi <774685342@qq.com>
2021-11-21 07:55:21 +0800

    easy-concat

--- questions/533-easy-concat/test-cases.ts
+++ questions/533-easy-concat/test-cases.ts
@@ -1,8 +1,16 @@
-import { Equal, Expect } from '@type-challenges/utils'
+import { Equal, Expect } from "../../utils";
+
+// 想复杂了， 可以使用一些js 的思想， 数组合并就是 ... 就OK了
+type Concat<T extends any[], U extends any[]> = [...T, ...U];
 
 type cases = [
   Expect<Equal<Concat<[], []>, []>>,
   Expect<Equal<Concat<[], [1]>, [1]>>,
   Expect<Equal<Concat<[1, 2], [3, 4]>, [1, 2, 3, 4]>>,
-  Expect<Equal<Concat<['1', 2, '3'], [false, boolean, '4']>, ['1', 2, '3', false, boolean, '4']>>,
-]
+  Expect<
+    Equal<
+      Concat<["1", 2, "3"], [false, boolean, "4"]>,
+      ["1", 2, "3", false, boolean, "4"]
+    >
+  >
+];
