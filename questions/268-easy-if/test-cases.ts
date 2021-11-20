08bf8746b756875a8361df3a5f419243a3be66f7
lansefengxinzi <774685342@qq.com>
2021-11-21 07:54:24 +0800

    easy-if

--- questions/268-easy-if/test-cases.ts
+++ questions/268-easy-if/test-cases.ts
@@ -1,9 +1,12 @@
-import { Equal, Expect } from '@type-challenges/utils'
+import { Equal, Expect } from "../../utils";
+
+// 条件判断
+type If<C, T, F> = C extends true ? T : F;
 
 type cases = [
-  Expect<Equal<If<true, 'a', 'b'>, 'a'>>,
-  Expect<Equal<If<false, 'a', 2>, 2>>,
-]
+  Expect<Equal<If<true, "a", "b">, "a">>,
+  Expect<Equal<If<false, "a", 2>, 2>>
+];
 
 // @ts-expect-error
-type error = If<null, 'a', 'b'>
+type error = If<null, "a", "b">;
