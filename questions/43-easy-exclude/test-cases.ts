da014cb78cdbfeb7ce8fa45daf3d10de308f2c9a
lansefengxinzi <774685342@qq.com>
2021-11-21 07:55:08 +0800

    easy-exclude

--- questions/43-easy-exclude/test-cases.ts
+++ questions/43-easy-exclude/test-cases.ts
@@ -1,7 +1,20 @@
-import { Equal, Expect } from '@type-challenges/utils'
+import { Equal, Expect } from "../../utils";
+
+// 联合类型的 extends 是对里面的每一项进行比较
+type MyExclude<T, U> = T extends U ? never : T;
 
 type cases = [
-    Expect<Equal<MyExclude<"a" | "b" | "c", "a">, Exclude<"a" | "b" | "c", "a">>>,
-    Expect<Equal<MyExclude<"a" | "b" | "c", "a" | "b">, Exclude<"a" | "b" | "c", "a" | "b">>>,
-    Expect<Equal<MyExclude<string | number | (() => void), Function>, Exclude<string | number | (() => void), Function>>>,
-]
+  Expect<Equal<MyExclude<"a" | "b" | "c", "a">, Exclude<"a" | "b" | "c", "a">>>,
+  Expect<
+    Equal<
+      MyExclude<"a" | "b" | "c", "a" | "b">,
+      Exclude<"a" | "b" | "c", "a" | "b">
+    >
+  >,
+  Expect<
+    Equal<
+      MyExclude<string | number | (() => void), Function>,
+      Exclude<string | number | (() => void), Function>
+    >
+  >
+];
