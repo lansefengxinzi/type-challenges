dc040cc0cad4c8838d88e128439a66c02bf262ec
lansefengxinzi <774685342@qq.com>
2021-11-21 07:03:59 +0800

    easy-readonly

--- questions/7-easy-readonly/test-cases.ts
+++ questions/7-easy-readonly/test-cases.ts
@@ -1,14 +1,17 @@
-import { Equal, Expect } from '@type-challenges/utils'
+import { Equal, Expect } from "../../utils";
 
-type cases = [
-  Expect<Equal<MyReadonly<Todo1>, Readonly<Todo1>>>,
-]
+// 可直接添加readonly
+type MyReadonly<T> = {
+  readonly [key in keyof T]: T[key];
+};
+
+type cases = [Expect<Equal<MyReadonly<Todo1>, Readonly<Todo1>>>];
 
 interface Todo1 {
-  title: string
-  description: string
-  completed: boolean
+  title: string;
+  description: string;
+  completed: boolean;
   meta: {
-    author: string
-  }
+    author: string;
+  };
 }
