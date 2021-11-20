ece11f116f54be36c75bc61cc4279119be9a4bcd
lansefengxinzi <774685342@qq.com>
2021-11-21 07:22:08 +0800

    11-easy-tuple-to-object

--- questions/11-easy-tuple-to-object/test-cases.ts
+++ questions/11-easy-tuple-to-object/test-cases.ts
@@ -1,10 +1,27 @@
-import { Equal, Expect } from '@type-challenges/utils'
+import { Equal, Expect } from "../../utils";
 
-const tuple = ['tesla', 'model 3', 'model X', 'model Y'] as const
+type TupleToObject<T extends readonly any[]> = {
+  [key in T[number]]: key;
+};
+
+const tuple = ["tesla", "model 3", "model X", "model Y"] as const;
+
+type s = typeof tuple;
+type s1 = [typeof tuple][number];
 
 type cases = [
-  Expect<Equal<TupleToObject<typeof tuple>, { tesla: 'tesla'; 'model 3': 'model 3'; 'model X': 'model X'; 'model Y': 'model Y'}>>,
-]
+  Expect<
+    Equal<
+      TupleToObject<typeof tuple>,
+      {
+        tesla: "tesla";
+        "model 3": "model 3";
+        "model X": "model X";
+        "model Y": "model Y";
+      }
+    >
+  >
+];
 
 // @ts-expect-error
-type error = TupleToObject<[[1, 2], {}]>
+type error = TupleToObject<[[1, 2], {}]>;
