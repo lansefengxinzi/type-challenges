e1fafbfe01348620df77caacc383138c1ec85cd5
lansefengxinzi <774685342@qq.com>
2021-11-21 07:54:55 +0800

    easy-awaited

--- questions/189-easy-awaited/test-cases.ts
+++ questions/189-easy-awaited/test-cases.ts
@@ -1,12 +1,15 @@
-import { Equal, Expect } from '@type-challenges/utils'
+import { Equal, Expect } from "../../utils";
 
-type X = Promise<string>
-type Y = Promise<{ field: number }>
+type X = Promise<string>;
+type Y = Promise<{ field: number }>;
+
+// 注意 infer 的使用， 可以用infer 重新定义类型， 获取类型
+type Awaited<T extends Promise<any>> = T extends Promise<infer U> ? U : never;
 
 type cases = [
   Expect<Equal<Awaited<X>, string>>,
-  Expect<Equal<Awaited<Y>, { field: number }>>,
-]
+  Expect<Equal<Awaited<Y>, { field: number }>>
+];
 
 // @ts-expect-error
-type error = Awaited<number>
+type error = Awaited<number>;
